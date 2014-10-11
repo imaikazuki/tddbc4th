853455dcd10f2596274fcc4134b3f72f591f7678
Kazuki IMAI <imaikazuki@gmx.com>
2014-10-11 15:54:25 +0900

    性別を実装した。

--- src/kotlin/tddbc4th/Person.kt
+++ src/kotlin/tddbc4th/Person.kt
@@ -1,8 +1,9 @@
 package tddbc4th
 
-class 人(_名字 : String, _名前 : String) {
+class 人(_名字 : String, _名前 : String, _性別 : 性別型) {
   val 名字 = if(myTrim(_名字).equals("")) { throw RuntimeException() } else { myTrim(_名字) }
   val 名前 = if(myTrim(_名前).equals("")) { throw RuntimeException() } else { myTrim(_名前) }
+  val 性別 = _性別
 
   fun 氏名() : String {
     return 名字 + 名前
@@ -11,4 +12,9 @@ class 人(_名字 : String, _名前 : String) {
 
 fun myTrim(str : String) : String {
   return str.replaceAll("^[ ¥t¥r¥n　]*", "").replaceAll("[ ¥t¥r¥n　]*$", "")
+}
+
+enum class 性別型 {
+  男性
+  女性
 }
--- test/kotlin/tddbc4th/PersonTest.kt
+++ test/kotlin/tddbc4th/PersonTest.kt
@@ -7,29 +7,29 @@ import kotlin.test.fail
 
 class 人Test {
   test fun 名字Test() {
-    val 佐藤 = 人("佐藤", "ほげほげ")
+    val 佐藤 = 人("佐藤", "ほげほげ", 性別型.男性)
     assertEquals("佐藤", 佐藤.名字)
   }
 
   test fun 名前Test() {
-    val 太郎 = 人("ぴよぴよ", "太郎")
+    val 太郎 = 人("ぴよぴよ", "太郎", 性別型.男性)
     assertEquals("太郎", 太郎.名前)
   }
 
   test fun 氏名Test() {
-    val 佐藤太郎 = 人("佐藤", "太郎")
+    val 佐藤太郎 = 人("佐藤", "太郎", 性別型.男性)
     assertEquals("佐藤太郎", 佐藤太郎.氏名())
   }
 
   test fun 違う人の氏名Test() {
-    val 鈴木一郎 = 人("鈴木", "一郎")
+    val 鈴木一郎 = 人("鈴木", "一郎", 性別型.男性)
     assertEquals("鈴木一郎", 鈴木一郎.氏名())
   }
 
   test fun 名字が空であれば例外Test() {
     // 言語レベルで引数にNULLが入りません
     try {
-      val person = 人("", "ほげほげ")
+      val person = 人("", "ほげほげ", 性別型.男性)
       fail("名字が空でもインスタンスが返された")
     } catch ( e : RuntimeException ) {
     }
@@ -38,7 +38,7 @@ class 人Test {
   test fun 名前が空であれば例外Test() {
     // 言語レベルで引数にNULLが入りません
     try {
-      val person = 人("ほげほげ", "")
+      val person = 人("ほげほげ", "", 性別型.男性)
       fail("名前が空でもインスタンスが返された")
     } catch ( e: RuntimeException ) {
     }
@@ -46,7 +46,7 @@ class 人Test {
 
   test fun 名字が空白文字のみであれば例外Test() {
     try {
-      val person = 人(" ¥t¥n¥r　", "ほげほげ")
+      val person = 人(" ¥t¥n¥r　", "ほげほげ", 性別型.男性)
       fail("名字が空白文字でもインスタンスが返された")
     } catch ( e : RuntimeException ) {
     }
@@ -54,7 +54,7 @@ class 人Test {
 
   test fun 名字の字の間に空白文字入ってる人はゆるすTest() {
     try {
-      val person = 人("今 ¥t¥n¥r　井", "ほげほげ")
+      val person = 人("今 ¥t¥n¥r　井", "ほげほげ", 性別型.男性)
       assertEquals("今 ¥t¥n¥r　井", person.名字)
     } catch ( e : RuntimeException ) {
     }
@@ -62,7 +62,7 @@ class 人Test {
 
   test fun 名前が空白文字のみであれば例外Test() {
     try {
-      val person = 人("あれこれ", " ¥t¥n¥r　")
+      val person = 人("あれこれ", " ¥t¥n¥r　", 性別型.男性)
       fail("名前が空白文字でもインスタンスが返された")
     } catch ( e : RuntimeException ) {
     }
@@ -70,7 +70,7 @@ class 人Test {
 
   test fun 名前の字の間に空白文字入ってる人はゆるすTest() {
     try {
-      val person = 人("あれこれ", "太 ¥t¥n¥r　郎")
+      val person = 人("あれこれ", "太 ¥t¥n¥r　郎", 性別型.男性)
       assertEquals("太 ¥t¥n¥r　郎", person.名前)
     } catch ( e : RuntimeException ) {
     }
@@ -83,5 +83,15 @@ class 人Test {
     assertEquals("先頭にtabを含む文字列", myTrim("¥t先頭にtabを含む文字列"))
     assertEquals("先頭に改行を含む文字列", myTrim("¥n先頭に改行を含む文字列"))
     assertEquals("先頭にキャリッジを含む文字列", myTrim("¥r先頭にキャリッジを含む文字列"))
+  }
+
+  test fun 男性を作れるTest() {
+    val person = 人("名字", "名前", 性別型.男性)
+    assertEquals(性別型.男性, person.性別)
+  }
+
+  test fun 女性を作れるTest() {
+    val person = 人("名字", "名前", 性別型.女性)
+    assertEquals(性別型.女性, person.性別)
   }
 }
